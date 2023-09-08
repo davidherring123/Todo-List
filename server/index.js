d32a3029410d2cfdb679a52f106fb560769d544d
davidherring123 <david.herring8680@gmail.com>
2023-09-08 10:59:02 -0500

    Added Create, Get, Update, and Delete functionality with todos in database.

--- server/index.js
+++ server/index.js
@@ -6,13 +6,12 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json());
 
-
 //CREATE
 
 app.post("/todos", async (req, res) => {
     try {
         const { description } = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1)", 
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", 
         [description]);
 
         res.json(newTodo);
@@ -21,8 +20,59 @@ app.post("/todos", async (req, res) => {
     }
 })
 
+//GET all todos
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+
+//GET a todo
+
+app.get("/todos:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+//UPDATE a todo
+
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+
+        res.json("Todo was Updated");
+    } catch (err) {
+        console.error(err);
+    }
+ });
+
+ //DELETE a todo
+
+ app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [ id]);
+
+        res.json("Todo was Deleted");
+    } catch (err) {
+        console.error(err);
+    }
+ });
+
 
 app.listen(5001, () => {
-    console.log("Server is starting on port 5000");
+    console.log("Server is starting on port 5001");
 });
 
